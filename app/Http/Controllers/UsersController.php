b3b3fb560ddfd948e9a87c548efa2979f22c7ec7
zhujianghan <zhujianghan@gmail.com>
2019-03-09 23:30:45 +0800

    登录成功后导航逻辑

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
-
+use Auth;
 class UsersController extends Controller
 {
     public function create()
@@ -31,6 +31,7 @@ class UsersController extends Controller
             'password' => bcrypt($request->password)
         ]);
 
+        Auth::login($user);
         session()->flash('success', '欢迎, 您将在这里开启一段新的旅程.');
         return redirect()->route('users.show', [$user]);
 //        return redirect()->route('users.show', [$user->id]);// route() 方法会自动获取 Model 的主键
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -2,12 +2,37 @@
     <div class="container">
         <a class="navbar-brand" href="{{ route('home') }}">Wi App</a>
         <ul class="navbar-nav justify-content-end">
-            <li class="nav-item">
-                <a href="{{ route('help') }}" class="nav-link">帮助</a>
-            </li>
-            <li class="nav-item">
-                <a href="#" class="nav-link">登录</a>
-            </li>
+            @if(Auth::check())
+                {{--已登录时--}}
+                <li class="nav-item"><a class="nav-link" href="#">用户列表</a></li>
+                <li class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" id="navbarDropdown" role="button"
+                       data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        {{ Auth::user()->name }}
+                    </a>
+
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a href="{{ route('users.show', Auth::user()) }}" class="dropdown-item">个人中心</a>
+                        <a href="#" class="dropdown-item">编辑资料</a>
+                        <div class="dropdown-divider"></div>
+                        <a href="#" class="dropdown-item" id="logout">
+                            <form action="{{ route('logout',Auth::user()) }}" method="post">
+                                {{ csrf_field() }}
+                                {{ method_field('delete') }}
+                                <button type="submit" class="btn btn-block btn-danger">退出登录</button>
+                            </form>
+                        </a>
+                    </div>
+                </li>
+            @else
+                {{--未登录时--}}
+                <li class="nav-item">
+                    <a href="{{ route('help') }}" class="nav-link">帮助</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">登录</a>
+                </li>
+            @endif
         </ul>
     </div>
 </nav>
