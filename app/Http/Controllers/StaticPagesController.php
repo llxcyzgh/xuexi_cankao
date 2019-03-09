58f240facf4f44d402d0f294c43e09cf16904626
zhujianghan <zhujianghan@gmail.com>
2019-03-09 17:44:57 +0800

    basic pages

---
+++ app/Http/Controllers/StaticPagesController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StaticPagesController extends Controller
+{
+    public function home()
+    {
+        // return 'homepage';// 把内容直接显示在页面
+        return view('static_pages/home');// 通过视图显示页面
+    }
+
+    public function help()
+    {
+        return view('static_pages/help');
+    }
+
+    public function about()
+    {
+        return view('static_pages/about');
+    }
+
+
+}
---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>@yield('title','Weibo App') - xuexi 参考答案</title>
+</head>
+<body>
+@yield('content')
+</body>
+</html>
