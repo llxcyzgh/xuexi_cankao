87103d94c55f5875696b397117e437cd9e57b1ae
zhujianghan <zhujianghan@gmail.com>
2019-03-09 21:10:23 +0800

    用户显示页面

--- app/Models/User.php
+++ app/Models/User.php
@@ -39,4 +39,10 @@ class User extends Authenticatable
 
     // 指定数据表名
     protected $table = 'users';
+
+    public function gravatar($size = '100')
+    {
+        $hash = md5(strtolower(trim($this->attributes['email'])));
+        return "http://www.gravatar.com/avatar/{$hash}?s={$size}";
+    }
 }
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -10,9 +10,9 @@
 <div class="container">
     <div class="offset-md-1 col-md-10">
         @yield('content')
-
         @include('layouts._footer')
     </div>
+
 
 </div>
 
