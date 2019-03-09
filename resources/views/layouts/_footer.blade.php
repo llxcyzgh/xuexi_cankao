e61e475b1db6a4f5424d7978fca1db72b8f5be67
zhujianghan <zhujianghan@gmail.com>
2019-03-09 19:19:42 +0800

    切割头部和尾部子视图“

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+    <img src="http://g.alicdn.com/yp/party-login/0.1.6/favicon.png" alt="" class="brand-icon" width="20px">
+    <a href="http://xuexi.cn" target="_blank">xuexi 强国</a>
+
+    <div class="float-right">
+        <a href="/about">关于</a>
+    </div>
+</footer>
