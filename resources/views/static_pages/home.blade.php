b30f7f389cde97d8bd9fca61005e0236eb3abc1c
zhujianghan <zhujianghan@gmail.com>
2019-03-09 19:31:58 +0800

    用户注册页面

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -6,6 +6,6 @@
         <h1>hello Wi</h1>
         <p class="lead">你现在位于 Wi app 的首页</p>
         <p>學習, 从这里开始</p>
-        <p class="btn btn-lg btn-success">现在注册</p>
+        <a href="{{ route('signup') }}" class="btn btn-lg btn-success">现在注册</a>
     </div>
 @endsection
